7a6c68414e85be05c02cef5469b17c3f037e0856
Vignesh Sankaran <developer@sankaran.io>
2017-05-09 19:08:43 +1000

    Cleanup of distance matrix request code and restructuring of integration tests

--- src/distance_matrix/mod.rs
+++ src/distance_matrix/mod.rs
@@ -1,6 +1,5 @@
 use super::request_structs::LatLng;
 use super::types::TravelMode;
-use self::request::DistanceMatrixRequest;
 use self::response::DistanceMatrixResponse;
 use std::io::Read;
 use hyper;
@@ -10,56 +9,52 @@ use hyper_native_tls::NativeTlsClient;
 use serde_urlencoded;
 use serde_json;
 
-mod request;
 mod response;
-/*
-    Using this mod file means this is the only place I can
-    declare all other modules needed i.e. I can't nest modules
-*/
 mod duration_seconds;
 
 // Default use JSON
 const URL_EXTENSION: &'static str = "distancematrix/json";
 
-// This is probably not thread safe but we'll worry about that later
-fn request(request_data: DistanceMatrixRequest) -> DistanceMatrixResponse {
-    // Safe to unwrap since external code that doesn't pass in any values will not compile
-    let request_params_encoded = serde_urlencoded::to_string(&request_data).unwrap();
-    let request_url = super::BASE_URL.to_string() + URL_EXTENSION + "?" + request_params_encoded.as_str();
-    let processed_url = hyper::Url::parse(&request_url).unwrap();
-    
-    let ssl = NativeTlsClient::new().unwrap();
-    let ssl_connector = HttpsConnector::new(ssl);
-    let client = Client::with_connector(ssl_connector);
+#[derive(Serialize)]
+pub struct DistanceMatrixRequest {
+    origins: String,
+    destinations: String,
+    api_key: String,
+    travel_mode: Option<TravelMode>,
+}
 
-    let mut response = client.get(processed_url).send().unwrap();
-    let mut response_string = String::new();
-    {
-        response.read_to_string(&mut response_string).unwrap();
+impl DistanceMatrixRequest {
+    pub fn new(self, origins: String, destinations: String, api_key: String) {
+        self.origins = origins;
+        self.destinations = destinations;
+        self.api_key = api_key;
+        self.travel_mode = None;
     }
-    let distance_matrix_response: DistanceMatrixResponse = serde_json::from_str(&response_string).unwrap();
 
-    distance_matrix_response
-}
+    pub fn set_travel_mode(self, travel_mode: TravelMode) {
+        self.travel_mode = Some(travel_mode);
+    }
 
-pub fn lat_lng_request(api_key: String, origin: LatLng, destination: LatLng) -> DistanceMatrixResponse {
-    let request_data = DistanceMatrixRequest {
-        origins: origin.to_string(),
-        destinations: destination.to_string(),
-        api_key: api_key,
-        travel_mode: None,
-    };
+    // This is probably not thread safe but we'll worry about that later
+    pub fn send(self) -> DistanceMatrixResponse {
+        // Safe to unwrap since external code that doesn't pass in any values will not compile
+        let request_params_encoded = serde_urlencoded::to_string(&self).unwrap();
+        let request_url = super::BASE_URL.to_string() + URL_EXTENSION + "?" +
+                          request_params_encoded.as_str();
+        let processed_url = hyper::Url::parse(&request_url).unwrap();
 
-    request(request_data)
-}
+        let ssl = NativeTlsClient::new().unwrap();
+        let ssl_connector = HttpsConnector::new(ssl);
+        let client = Client::with_connector(ssl_connector);
 
-pub fn address_request(api_key: String, origin: String, destination: String) -> DistanceMatrixResponse {
-    let request_data = DistanceMatrixRequest {
-        origins: origin,
-        destinations: destination,
-        api_key: api_key,
-        travel_mode: None,
-    };
+        let mut response = client.get(processed_url).send().unwrap();
+        let mut response_string = String::new();
+        {
+            response.read_to_string(&mut response_string).unwrap();
+        }
+        let distance_matrix_response: DistanceMatrixResponse =
+            serde_json::from_str(&response_string).unwrap();
 
-    request(request_data)
+        distance_matrix_response
+    }
 }
--- tests/distance_matrix.rs
+++ tests/distance_matrix.rs
@@ -3,26 +3,8 @@ extern crate dotenv;
 
 use dotenv::dotenv;
 use google_maps_services::request_structs::LatLng;
-use google_maps_services::distance_matrix::lat_lng_request;
-use google_maps_services::distance_matrix::address_request;
+use google_maps_services::types::TravelMode;
 use std::env;
-
-#[test]
-fn lat_lng() {
-    // Get API key from .env file
-    dotenv().ok();
-    let api_key = env::var("API_KEY").expect("API_KEY must be set in the .env file");
-
-    // Melbourne CBD
-    let origin = LatLng::new(-37.8274812, 144.9352466);
-
-    // Ballarat CBD
-    let destination = LatLng::new(-37.5674314, 143.7827008);
-
-    let result = lat_lng_request(api_key, origin, destination);
-
-    assert_eq!(result.status, "OK");
-}
 
 #[test]
 fn address_minimum() {
@@ -32,7 +14,9 @@ fn address_minimum() {
     let origin = "Melbourne GPO".to_string();
     let destination = "Ballarat CBD".to_string();
 
-    let result = address_request(api_key, origin, destination);
+    let request = request::new(origin, destination, api_key);
+
+    let result = request.send();
 
     assert_eq!(result.status, "OK");
 }
@@ -40,5 +24,16 @@ fn address_minimum() {
 #[test]
 fn address_travel_mode() {
     dotenv().ok();
-    unimplemented!();
+    let api_key = env::var("API_KEY").expect("API_KEY must be set in the .env file");
+
+    let origin = "Melbourne GPO".to_string();
+    let destination = "Ballarat CBD".to_string();
+    let travel_mode = TravelMode::Transit;
+
+    let request = request::new(origin, destination, api_key);
+    request.set_travel_mode(travel_mode);
+
+    let result = request.send();
+
+    assert_eq!(result.status, "OK");
 }
