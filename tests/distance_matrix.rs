589f7fd4202f50dab75204dc22c889a269d9f5c3
Vignesh <developer@sankaran.io>
2017-06-12 21:43:48 +1000

    More idiomatic error test implemented

--- tests/distance_matrix.rs
+++ tests/distance_matrix.rs
@@ -22,6 +22,7 @@ fn address_minimum() {
     let result = request.send();
 
     assert_eq!(result.status, TopLevelStatus::Ok);
+    // TODO: Insert assertions against the other response fields too
 }
 
 #[test]
@@ -61,7 +62,6 @@ fn address_transit_mode_success() {
 }
 
 #[test]
-#[should_panic]
 fn address_transit_mode_fail() {
     dotenv().ok();
     let api_key = env::var("API_KEY").expect("API_KEY must be set in the .env file");
@@ -69,8 +69,20 @@ fn address_transit_mode_fail() {
     let origin = "Melbourne GPO".to_string();
     let destination = "Ballarat CBD".to_string();
 
-    let _ = DistanceMatrixRequestBuilder::new(origin, destination, api_key)
+    /* 
+        TODO: See if there's another way to check if an error has occurred and
+        throw a panic when the expected error occurs
+    */
+    let builder = DistanceMatrixRequestBuilder::new(origin, destination, api_key)
                         .set_travel_mode(TravelMode::Bicycling)
-                        .set_transit_mode(TransitMode::Rail).unwrap()
-                        .create();
+                        .set_transit_mode(TransitMode::Rail);
+
+    match builder {
+        Err(e) => {
+            assert_eq!(e, google_maps_services::distance_matrix::error::IncompatibleTravelModeError);
+        },
+        Ok(_) => {
+            panic!("There should be an error here");
+        },
+    }
 }
